9ce5897225f63e00524144b0b50cb524977ef0a0
geekyparth2004 <154241652+geekyparth2004@users.noreply.github.com>
2025-12-15 21:27:55 +0530

    Solved Modulus Opeartor

---
+++ Input-and-Operators/Modulus-Opeartor.java
@@ -0,0 +1,11 @@
+public class Modulus
+{
+    public static void main(String[] args) {
+        int n = 10;
+        int div = 2;
+
+        int val = n%div;
+
+        System.out.println(val);
+    }
+}
