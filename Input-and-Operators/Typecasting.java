1d18bec35f0af50a10ef3599f4e1e45bf942284b
geekyparth2004 <154241652+geekyparth2004@users.noreply.github.com>
2025-12-15 23:02:45 +0530

    Solved Typecasting

---
+++ Input-and-Operators/Typecasting.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        double s = (double) n;
+
+        System.out.println(n);
+        System.out.println(s);
+    }
+}
