ef8edb68b73649b4d3f812cf32307104a5d6ba0c
geekyparth2004 <154241652+geekyparth2004@users.noreply.github.com>
2025-12-21 22:32:12 +0530

    Solved Print all odd number

---
+++ Loops/Print-all-odd-number.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Print_all_odd_number {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10 ; i++)
+        {
+            if(i%2 != 0) System.out.println(i);
+        }
+    }
+}
