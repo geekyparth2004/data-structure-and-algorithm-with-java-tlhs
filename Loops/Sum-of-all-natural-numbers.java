3bd37713f99cf090a59cfcd6a22ae9131e2bc613
geekyparth2004 <154241652+geekyparth2004@users.noreply.github.com>
2025-12-21 22:33:59 +0530

    Solved Sum of all natural numbers

---
+++ Loops/Sum-of-all-natural-numbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Sum_of_all_natural_numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); 
+        int sum = 0;
+
+        for(int i = 1; i <= n ; i++)
+        {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
