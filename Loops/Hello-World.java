58b275b5f9ce2b7d58c8bb94f946c666bda9346f
geekyparth2004 <154241652+geekyparth2004@users.noreply.github.com>
2025-12-21 21:47:46 +0530

    Solved Hello World

---
+++ Loops/Hello-World.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Hello_World
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1 ; i <= n ; i++)
+        {
+            System.out.println("Hello World");
+        }
+    }
+}
