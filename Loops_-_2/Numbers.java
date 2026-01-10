7e35a59269245e4178a93c82d8cf6677a3d4e222
geekyparth2004 <154241652+geekyparth2004@users.noreply.github.com>
2026-01-10 11:15:59 +0530

    Solved Numbers

---
+++ Loops_-_2/Numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Numbers {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while(i <= 10)
+        {
+            System.out.print(i);
+            System.out.print(" ");
+            i++;
+        }
+    }
+}
