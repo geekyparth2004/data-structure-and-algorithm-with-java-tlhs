728dbaf67ed1268d122617d413262375511ee0f3
geekyparth2004 <154241652+geekyparth2004@users.noreply.github.com>
2026-01-16 13:12:36 +0530

    Solved Product of Array

---
+++ Arrays/Product_of_Array.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); 
+
+        int[] arr = new int[n]; 
+
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            arr[i] = sc.nextInt(); 
+        }
+
+        int pro = 1; 
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            pro = pro * arr[i];
+        }
+
+        System.out.println(pro);
+        return;
+    }
+}
