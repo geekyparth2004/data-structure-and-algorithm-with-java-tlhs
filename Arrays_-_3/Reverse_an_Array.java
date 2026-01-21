9cb609e9884b9f5747d47821c899e7d8729b168d
geekyparth2004 <154241652+geekyparth2004@users.noreply.github.com>
2026-01-21 09:57:24 +0530

    Solved Reverse an Array

---
+++ Arrays_-_3/Reverse_an_Array.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            arr[i] = sc.nextInt(); 
+        }
+
+        // swap using two pointers
+
+        int i = 0;
+        int j = n-1;
+
+        while(i <= j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+        }
+
+        for(int ele : arr)
+        {
+            System.out.print(ele + " ");
+        }
+    }
+}
