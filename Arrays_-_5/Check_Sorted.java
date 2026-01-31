48ecc75f0ccc31c854b575c64a290620c9613643
geekyparth2004 <154241652+geekyparth2004@users.noreply.github.com>
2026-01-31 15:11:25 +0530

    Solved Check Sorted

---
+++ Arrays_-_5/Check_Sorted.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); 
+
+        int arr[] = new int[n];
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            arr[i] = sc.nextInt();
+        }        
+
+        int val = arr[0];
+
+        for(int i = 1 ; i < n ; i++)
+        {
+            if(arr[i] < val) 
+            {
+                System.out.println(false);
+                return;
+            } 
+
+            else
+            {
+                val = arr[i];
+            }
+        }
+
+        System.out.println(true);
+    }
+}
